72b7547c1a239b8e749104be3357e9f3473b64ed
Simkora <simkora1996@gmail.com>
2021-03-03 11:02:55 +0300

    Complete 1 and 2

--- src/HW_02_03_2021/TaskThree.java
+++ src/HW_02_03_2021/TaskThree.java
@@ -1,4 +1,27 @@
 package HW_02_03_2021;
 
 public class TaskThree {
+    public static void main(String[] args) {
+        cross(12);
+    }
+    static void cross(int number){
+        int j = 2;
+        int result = 0;
+        while (j <= number / 2)
+        {
+            if (number % j == 0)
+            {
+                result = 1;
+            }
+            j++;
+        }
+        if (result == 1)
+        {
+            System.out.println("Number: " + number + " is Not Prime.");
+        }
+        else
+        {
+            System.out.println("Number: " + number + " is Prime. ");
+        }
+    }
 }
--- src/HW_02_03_2021/TaskTwo.java
+++ src/HW_02_03_2021/TaskTwo.java
@@ -1,4 +1,18 @@
 package HW_02_03_2021;
 
 public class TaskTwo {
+    public static void main(String[] args) {
+        chart(3);
+        System.out.println();
+        chart(4);
+        System.out.println();
+        chart(2);
+    }
+    static void chart(int x) {
+        for (int j = x; j <= x && j>=0; j--) {
+            for (int i = x; i > j; i--) {
+                System.out.print(x - j  +" ");
+            }
+        }
+    }
 }
