01ac4b2785c6b870563b8b09907250ae6d074ce2
Simkora <simkora1996@gmail.com>
2021-03-03 08:59:50 +0300

    Complete TaskOne

--- src/HW_02_03_2021/TaskOne.java
+++ src/HW_02_03_2021/TaskOne.java
@@ -2,6 +2,11 @@ package HW_02_03_2021;
 
 public class TaskOne {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(sum(8, 11));
+        System.out.println(sum(1, 2));
+        System.out.println(sum(5, 5));
+    }
+    static int sum(int x, int y){
+        return x+y;
     }
 }
